348ebf02d4537373c5f71abad0ab42b943c84308
RGB00255 <RGB00255@users.noreply.github.com>
2020-03-21 21:00:49 -0400

    Added KeyDown events

--- Space Invaders/Space Invaders/GameWindow1.Designer.cs
+++ Space Invaders/Space Invaders/GameWindow1.Designer.cs
@@ -28,13 +28,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.alienSpeed = new System.Windows.Forms.Timer(this.components);
             this.timerCount = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.playerMovement = new System.Windows.Forms.Timer(this.components);
+            this.xLocation = new System.Windows.Forms.Label();
+            this.invaderTest = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invaderTest)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 188);
+            this.button1.Location = new System.Drawing.Point(251, 163);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 23);
+            this.button1.Size = new System.Drawing.Size(134, 20);
             this.button1.TabIndex = 0;
             this.button1.Text = "increase speed";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,9 +49,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(362, 227);
+            this.label1.Location = new System.Drawing.Point(310, 197);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "1";
             // 
@@ -58,22 +64,63 @@
             // timerCount
             // 
             this.timerCount.AutoSize = true;
-            this.timerCount.Location = new System.Drawing.Point(564, 196);
+            this.timerCount.ForeColor = System.Drawing.Color.White;
+            this.timerCount.Location = new System.Drawing.Point(483, 170);
             this.timerCount.Name = "timerCount";
-            this.timerCount.Size = new System.Drawing.Size(68, 15);
+            this.timerCount.Size = new System.Drawing.Size(57, 13);
             this.timerCount.TabIndex = 2;
             this.timerCount.Text = "timerCount";
             // 
+            // player
+            // 
+            this.player.Image = global::Space_Invaders.Properties.Resources.PlayerShip;
+            this.player.Location = new System.Drawing.Point(373, 769);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(52, 32);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // playerMovement
+            // 
+            this.playerMovement.Enabled = true;
+            this.playerMovement.Interval = 1;
+            this.playerMovement.Tick += new System.EventHandler(this.playerMovement_Tick);
+            // 
+            // xLocation
+            // 
+            this.xLocation.AutoSize = true;
+            this.xLocation.ForeColor = System.Drawing.Color.Coral;
+            this.xLocation.Location = new System.Drawing.Point(349, 400);
+            this.xLocation.Name = "xLocation";
+            this.xLocation.Size = new System.Drawing.Size(35, 13);
+            this.xLocation.TabIndex = 4;
+            this.xLocation.Text = "label2";
+            // 
+            // invaderTest
+            // 
+            this.invaderTest.Location = new System.Drawing.Point(149, 318);
+            this.invaderTest.Name = "invaderTest";
+            this.invaderTest.Size = new System.Drawing.Size(52, 32);
+            this.invaderTest.TabIndex = 5;
+            this.invaderTest.TabStop = false;
+            // 
             // GameWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 954);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(799, 827);
+            this.Controls.Add(this.invaderTest);
+            this.Controls.Add(this.xLocation);
+            this.Controls.Add(this.player);
             this.Controls.Add(this.timerCount);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "GameWindow";
             this.Text = "Space Invaders";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invaderTest)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +132,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer alienSpeed;
         private System.Windows.Forms.Label timerCount;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer playerMovement;
+        private System.Windows.Forms.Label xLocation;
+        private System.Windows.Forms.PictureBox invaderTest;
     }
 }
 
--- Space Invaders/Space Invaders/GameWindow1.cs
+++ Space Invaders/Space Invaders/GameWindow1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input; // For key events
 
 namespace SpaceInvaders {
     public partial class GameWindow : Form {
@@ -19,24 +20,43 @@ namespace SpaceInvaders {
 
         public GameWindow() {
             InitializeComponent();
+            player.Image = Image.FromFile("resources/textures/PlayerShip.png"); // Load player ship (dunno if necessary)
         }
 
         private void button1_Click(object sender, EventArgs e) { //TEMPORARY BUTTON TO INCREASE ALIEN SPEED, NORMALLY DONE BY KILLING ALIENS
             speedMultiplier = speedMultiplier * 1.01;
             ++buttonCount;
             label1.Text = $"{buttonCount}"; //TEMPORARY LABEL TO DISPLAY HOW MANY ALIENS HAVE BEEN KILLED, MAX IS 55 IN NORMAL GAME
+            invaderTest.Image = Image.FromFile("resources/textures/Alien1_1.png"); // TEST IMAGE LOADING
         }
 
         private void alienSpeed_Tick(object sender, EventArgs e) {
             gameTicks = gameTicks * speedMultiplier;
             timerCount.Text = $"{gameTicks}"; //TEMPORARY LABEL TO TRACK SPEED MULTIPLIER
             if (gameTicks >= 50) {
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "quack.wav");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "resources/sounds/quack.wav");
                 System.Media.SoundPlayer sp = new System.Media.SoundPlayer(path);
                 sp.Play();
                 gameTicks = 0;
             }
             ++gameTicks;
+        }
+
+        // Timer will handle all input from the user
+        private void playerMovement_Tick(object sender, EventArgs e)
+        {
+            xLocation.Text = player.Location.X.ToString(); // TEST DELETE ME
+
+            if (Keyboard.IsKeyDown(Key.Left)) // Move left
+            {
+                if(player.Location.X > 0)
+                    player.Location = new Point(player.Location.X - 2, player.Location.Y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Right)) // Move right
+            {
+                if(player.Location.X < 747) // May need to change this as we adjust the screen size
+                    player.Location = new Point(player.Location.X + 2, player.Location.Y);
+            }
         }
     }
 }
