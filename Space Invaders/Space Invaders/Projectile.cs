c8cadbf650292cb3d88bbec74828a2ef3866ee5b
nerdoftheweek01 <60940167+nerdoftheweek01@users.noreply.github.com>
2020-04-07 13:13:53 -0400

    Revert "BROKEN projectile class implimentation"
    This reverts commit 13e151bfb8ab01203cc00d2722b09e7d046e762b.

--- Space Invaders/Space Invaders/Projectile.cs
+++ Space Invaders/Space Invaders/Projectile.cs
@@ -9,8 +9,6 @@ namespace SpaceInvaders {
     class Projectile {
         private int type; // 0 = Alien, 1 = Player
         private Image projectileImage;
-        private bool vis;
-        private int xPos, yPos;
         public Projectile() {
 
         }
@@ -32,30 +30,5 @@ namespace SpaceInvaders {
             return projectileImage;
         }
 
-        public bool IsVisible()
-        {
-            return (vis);
-        }
-
-        public void SetVisibility(bool b)
-        {
-            vis = b;
-        }
-
-        public int GetPos(char dim) //gets the position of one dimension
-        {
-            if (dim == 'x')
-                return (xPos);
-            else
-                return (yPos);
-        }
-
-        public void SetPos(int pos, char dim) //sets the position of one dimension
-        {
-            if (dim == 'x')
-                xPos = pos;
-            else
-                yPos = pos;
-        }
     }
 }
