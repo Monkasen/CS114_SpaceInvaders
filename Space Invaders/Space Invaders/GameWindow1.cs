f81204a363551f57af6fc47982ae59daaaf97750
nerdoftheweek01 <60940167+nerdoftheweek01@users.noreply.github.com>
2020-04-07 13:13:37 -0400

    still broken

--- Space Invaders/Space Invaders/GameWindow1.cs
+++ Space Invaders/Space Invaders/GameWindow1.cs
@@ -85,7 +85,7 @@ namespace SpaceInvaders {
 
             if (!(p1.IsFired())) return;
             playerProjectile.Location = new Point(playerProj.GetPos('x'), playerProj.GetPos('y') - projectileSpeed);
-            playerProjectile.Location = new Point(playerProj.GetPos('x'), playerProj.GetPos('y') - projectileSpeed);
+            playerProj.SetPos(playerProj.GetPos('y') - projectileSpeed, 'y');
             p1.Fire(ProjectileEvent());
         }
 
