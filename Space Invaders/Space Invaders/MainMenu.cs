44e753aacd1f247edb2e3409df3acb038cdda213
nerdoftheweek01 <60940167+nerdoftheweek01@users.noreply.github.com>
2020-04-21 14:15:04 -0400

    fixed location

--- Space Invaders/Space Invaders/MainMenu.cs
+++ Space Invaders/Space Invaders/MainMenu.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input; // For key events
@@ -14,7 +15,26 @@ namespace SpaceInvaders {
         public static short gameMode = 0;
         public MainMenu() {
             InitializeComponent();
+            string fileName = Path.Combine(Environment.CurrentDirectory, "highscore.txt");
+            string[] scores = new string[] { "0", "0", "0" };
+            //if the file doesn't exist, create it
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+                for (int i = 0; i < 3; i++)
+                {
+                    File.WriteAllLines(fileName, scores);
+                }
+            }
+            using (StreamReader fileRead = new StreamReader(fileName))
+            {
+                scores[0] = fileRead.ReadLine();
+                scores[1] = fileRead.ReadLine();
+                scores[2] = fileRead.ReadLine();
+            }
         }
+
+        
 
         private void onePlayerButton_Click(object sender, EventArgs e) {
             gameMode = 1;
--- Space Invaders/Space Invaders/Program.cs
+++ Space Invaders/Space Invaders/Program.cs
@@ -17,24 +17,6 @@ namespace SpaceInvaders {
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainMenu());
 
-            string fileName = Path.Combine(Environment.CurrentDirectory, "highscore.txt");
-            string[] scores = new string[] { "0", "0", "0" };
-            //if the file doesn't exist, create it
-            if (!File.Exists(fileName))
-            {
-                File.Create(fileName);
-                for (int i = 0; i < 3; i++)
-                {
-                    File.WriteAllLines(fileName, scores);
-                }
-            }
-            using (StreamReader fileRead = new StreamReader(fileName))
-            {
-                scores[0] = fileRead.ReadLine();
-                scores[1] = fileRead.ReadLine();
-                scores[2] = fileRead.ReadLine();
-            }
-
             if (MainMenu.gameMode == 1) {
                 Application.Run(new GameWindow());
     }
